6910a60c79f07b82dc1ffd897530decdb1602abb
gibson314 <rye314@sina.com>
2017-03-27 00:16:34 -0500

    GraphClustering for test

---
+++ src/main/scala/keystoneml/nodes/graph/GraphConstruct.scala
@@ -0,0 +1,8 @@
+package keystoneml.nodes.graph
+
+/**
+  * Created by litian on 3/17/17.
+  */
+object GraphConstruct {
+
+}
---
+++ src/main/scala/keystoneml/nodes/graph/GraphText.java
@@ -0,0 +1,7 @@
+package keystoneml.nodes.graph;
+
+/**
+ * Created by litian on 3/17/17.
+ */
+public class GraphText {
+}
