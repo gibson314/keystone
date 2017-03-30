7766de6601223252995b82d255c75141bb23ce34
gibson314 <rye314@sina.com>
2017-03-30 10:24:20 -0500

    Test

--- runPubmed.sh
+++ runPubmed.sh
@@ -1,3 +1,4 @@
+export SPARK_HOME=/usr/local/spark
 KEYSTONE_MEM=4g ./bin/run-pipeline.sh \
 		  keystoneml.pipelines.nlp.PubmedPipeline \
 		    --textLocation ~/Pubmed/abstracts_text.tsv\
---
+++ src/main/scala/keystoneml/nodes/nlp/PubmedPreprocessor.java
@@ -0,0 +1,7 @@
+package keystoneml.nodes.nlp;
+
+/**
+ * Created by litian on 3/29/17.
+ */
+public class PubmedPreprocessor {
+}
